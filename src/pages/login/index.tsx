17ad5f6a3bd327a70f4d50473525dbf8fe02e9ba
HieuLe <lthieu9601@gmail.com>
2024-12-27 15:10:55 +0700

    feat: remove login init form

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,14 +1,5 @@
 import { AuthPage } from "@refinedev/antd";
 
 export const Login = () => {
-  return (
-    <AuthPage
-      type='login'
-      formProps={{
-        initialValues: { email: "admin@example.com", password: "password123" },
-      }}
-      registerLink={false}
-      rememberMe={false}
-    />
-  );
+  return <AuthPage type='login' registerLink={false} rememberMe={false} />;
 };
